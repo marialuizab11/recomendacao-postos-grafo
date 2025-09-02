71acc9f0cddf3dcab49b0cbb0357109b83bf284a
marialuizab11 <marialuizabsantos0@gmail.com>
2025-09-02 15:12:10 -0300

    adiciona classe para receber dados do front no back

--- recomendacao-postos/src/main/java/dto/RequisicaoCalculoDTO.java
+++ recomendacao-postos/src/main/java/dto/RequisicaoCalculoDTO.java
@@ -1,8 +1,27 @@
 package dto;
 
 /**
- * @author Maria Luiza Bezerra
+ * Representa os dados da requisição que chegam do frontend para cálculo da rota.
  */
 public class RequisicaoCalculoDTO {
+    private String idPartida;
+    private String idDestino;
+    private double autonomiaKmL;
+    private double litrosParaAbastecer;
+    
+    public String getIdPartida(){
+        return idPartida;
+    }
+    
+    public String getIdDestino(){
+        return idDestino;
+    }
+    
+    public double getAutonomiaKmL() {
+        return autonomiaKmL;
+    }
 
+    public double getLitrosParaAbastecer() {
+        return litrosParaAbastecer;
+    }
 }
